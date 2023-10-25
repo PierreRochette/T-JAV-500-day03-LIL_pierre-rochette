1c707d08ef15b8cd15865123e7aeab6bc31e4ddd
Pierre Rochette <pierre.rochette@epitech.eu>
2023-10-25 09:58:09 +0200

    task 2

---
+++ ex_02/Astronaut.java
@@ -0,0 +1,33 @@
+public class Astronaut {
+    private static int lastId=0;
+    private final int id;
+    private final String name;
+    private int snacks;
+    private String destination;
+
+    public Astronaut(String name) {
+        this.name = name;
+        this.id = lastId;
+        lastId++;
+        this.snacks = 0;
+        this.destination = null;
+        System.out.println(name + " ready for lunch!");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public int getSnacks() {
+        return snacks;
+    }
+
+}
