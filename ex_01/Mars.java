5491364335071fa5f90ade3970b5c26a3001c243
Pierre Rochette <pierre.rochette@epitech.eu>
2023-10-25 09:46:56 +0200

    task 1

---
+++ ex_01/Mars.java
@@ -0,0 +1,14 @@
+public class Mars {
+    private static int lastId=0;
+    private final int id;
+
+    public Mars() {
+        this.id = lastId;
+        lastId++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+}
